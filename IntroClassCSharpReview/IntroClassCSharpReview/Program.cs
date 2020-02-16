b170c9e13a5fdb448fd6855abd529d9e0b1aeeb6
miguel <micky.ale7@gmail.com>
2020-02-16 14:24:10 -0400

    added class example

---
+++ IntroClassCSharpReview/IntroClassCSharpReview/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace IntroClassCSharpReview
+{
+    public class Shape : Object
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+
+
+
+        public virtual void Show()
+        {
+            //Console.WriteLine(string.Format("My name is {0} shape", name));
+            Console.WriteLine($"My name is {Name} shape");       
+        }
+    }
+
+    
+    public class Square : Shape
+    {
+        public override void Show()
+        {
+            Console.WriteLine($"I'm a square {Name}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Shape myShape = new Shape("basic");
+            //var myShape = new Shape() { Name = "basic", Color = "blue" };
+            Shape myShape = new Square() { Name = "basic", Color = "blue" };
+            myShape.Show();
+
+            Console.WriteLine("Hello World!");
+        
+        }
+    }
+}
